b4d47055506f745d17bf1e87b21c82aa911c1760
bbha99 <branha99@gmail.com>
2023-01-09 19:32:00 -0800

    comparing tail values

--- tail.js
+++ tail.js
@@ -11,10 +11,13 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  array.shift()
-  return array
+  return array.slice(1, array.length);
 };
 
 // TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
