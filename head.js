f06df7b8992a90cf9bf494390345aa78c7dcbae6
bbha99 <branha99@gmail.com>
2023-01-09 19:19:30 -0800

    assert first element passes

--- head.js
+++ head.js
@@ -5,7 +5,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${fail} Assertion Failed: ${actual} === ${expected}`);
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
 
   }
 };
