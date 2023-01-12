ef1c8b8b9600b9090bd25b1c75f65b6c00a31c08
bbha99 <branha99@gmail.com>
2023-01-12 15:22:56 -0800

    creating our own map function

--- map.js
+++ map.js
@@ -1,5 +1,26 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("true")
+  } else {
+    console.log("false")
+  }
+};
+
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -10,3 +31,9 @@ const map = function(array, callback) {
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+const test1 = map(["bear"], word => word[0]);
+assertArraysEqual(test1, ["b"]);
+
+const test2 = map([], word => word[0]);
+assertArraysEqual(test2, []);
