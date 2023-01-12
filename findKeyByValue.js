2847389bdcf345e673c96a63def1c80f51309ccb
bbha99 <branha99@gmail.com>
2023-01-11 20:28:55 -0800

    find key name by their value

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let fail = String.fromCodePoint(0x1F621);
+  let pass = String.fromCodePoint(0x1F525);
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  let keyFound;
+  for (let key in obj) {
+    if (obj[key] === val) {
+      keyFound = key;
+      return keyFound
+    }
+  }
+  return keyFound;
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+// Object.keys also would work with for...of
