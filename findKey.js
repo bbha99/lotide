d597e652d28712b88b990164c5add45c4cec5080
bbha99 <branha99@gmail.com>
2023-01-12 16:16:23 -0800

    find particular star key

---
+++ findKey.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let fail = String.fromCodePoint(0x1F621);
+  let pass = String.fromCodePoint(0x1F525);
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (true === callback(object[key])) {
+      return key;
+    }
+  }
+  return;
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
