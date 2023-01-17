6a63f1252f7d07ca985f806391f4ef5f6bbeeb79
bbha99 <branha99@gmail.com>
2023-01-16 20:53:56 -0800

    Added findKeyByValue function for export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,14 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let fail = String.fromCodePoint(0x1F621);
-  let pass = String.fromCodePoint(0x1F525);
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const findKeyByValue = function(obj, val) {
   let keyFound;
@@ -21,14 +11,14 @@ const findKeyByValue = function(obj, val) {
   return keyFound;
 };
 
+module.exports = findKeyByValue;
+// const bestTVShowsByGenre = { 
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 // Object.keys also would work with for...of
