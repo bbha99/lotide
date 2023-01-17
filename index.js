5e77c2eec6f04955eec7cf0e8739824f2044fca5
bbha99 <branha99@gmail.com>
2023-01-16 22:35:46 -0800

    final clean

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
-const countLetters = require('./countLetters');
-const countOnly = require("./countOnly");
+const assertArraysEqual = require('./assertArraysEqual');
 const eqObjects = require("./eqObjects");
 const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require('./countLetters');
+const countOnly = require("./countOnly");
 const findKey = require("./findKey");
 const findKeyByValue = require("./findKeyByValue");
 const flatten = require("./flatten");
@@ -21,13 +21,13 @@ module.exports = {
   head,
   tail,
   middle,
-  assertArraysEqual,
   assertEqual,
   eqArrays,
-  countLetters,
-  countOnly,
+  assertArraysEqual,
   eqObjects,
   assertObjectsEqual,
+  countLetters,
+  countOnly,
   findKey,
   findKeyByValue,
   flatten,
