8373249ddf8fd94744fcba1b18c7d01f95b0c400
bbha99 <branha99@gmail.com>
2023-01-16 20:33:39 -0800

    cleaned assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -2,12 +2,10 @@
 const assertEqual = function(actual, expected) {
   let fail = String.fromCodePoint(0x1F621);
   let pass = String.fromCodePoint(0x1F525);
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  if (actual !== expected) {
     console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-
   }
+  console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
 };
 
 module.exports = assertEqual;
