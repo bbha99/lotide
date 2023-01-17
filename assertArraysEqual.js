0325214cdc55b50afe64909a2132774eba6b6174
bbha99 <branha99@gmail.com>
2023-01-16 20:32:40 -0800

    cleaned assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,9 @@
 const eqArrays = require("./eqArrays");
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log("true")
-  } else {
-    console.log("false")
+  if (eqArrays(arr1, arr2) !== true) {
+    console.log("false");
   }
+  console.log("true");
 };
 
 module.exports = assertArraysEqual;
-
-// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
