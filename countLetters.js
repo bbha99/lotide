c467883c79c3ec320bcb91e3fa5ee4a5abae16ea
bbha99 <branha99@gmail.com>
2023-01-16 20:35:20 -0800

    added countLetters function to exports

--- countLetters.js
+++ countLetters.js
@@ -1,15 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let fail = String.fromCodePoint(0x1F621);
-  let pass = String.fromCodePoint(0x1F525);
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
-
 const countLetters = function(sentence) {
   result = {};
   const noSpaceStr = sentence.split(" ").join("");
@@ -23,4 +11,5 @@ const countLetters = function(sentence) {
   return result;
 };
 
-console.log(countLetters("lighthouse in the house"))
+// console.log(countLetters("lighthouse in the house"))
+module.exports = countLetters;
