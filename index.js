6ea95d7e1f70933fdee1e3f856d99d31c00d4ef1
bbha99 <branha99@gmail.com>
2023-01-16 21:06:08 -0800

    index file containing all utility functions

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const countLetters = require('./countLetters');
+const countOnly = require("./countOnly");
+const eqObjects = require("./eqObjects");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  assertObjectsEqual: assertObjectsEqual,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
