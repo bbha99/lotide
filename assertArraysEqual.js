6444958c7f9785c03fc03572c363448b48b50e56
bbha99 <branha99@gmail.com>
2023-01-16 18:52:41 -0800

    refactored middle.js and added test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,6 +7,8 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
+module.exports = assertArraysEqual;
+
 // assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
 // assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
--- middle.js
+++ middle.js
@@ -1,26 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  console.log("this:", actual, expected)
-  if (eqArrays(actual, expected) === true) {
-    console.log("true")
-  } else {
-    console.log("false")
-  }
-};
-
 const middle = function(array) {
   let middleArr = [];
   if (array.length <= 2) {
@@ -36,6 +14,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+module.exports = middle;
